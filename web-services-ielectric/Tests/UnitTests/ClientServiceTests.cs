2f0ba978f01fb2d25c041236cf357ef1fe22a09e
JulioAsillo <u20201b482@upc.edu.pe>
2023-09-26 17:07:11 -0500

    add more test unit

---
+++ web-services-ielectric/Tests/UnitTests/ClientServiceTests.cs
@@ -0,0 +1,129 @@
+﻿using Xunit;
+using Moq;
+using web_services_ielectric.Clients.Domain.Models;
+using web_services_ielectric.Clients.Domain.Repositories;
+using web_services_ielectric.Clients.Services;
+using web_services_ielectric.Shared.Domain.Repositories;
+
+namespace web_services_ielectric.Tests.UnitTests;
+
+public class ClientServiceTests
+{
+    [Fact]
+    public async Task GetByIdAsync_ExistingClient_ReturnsClientResponseWithClient()
+    {
+        // Arrange
+        var clientId = 1;
+        var existingClient = new Client { Id = clientId, Names = "John", LastNames = "Doe" };
+        var clientRepositoryMock = new Mock<IClientRepository>();
+        clientRepositoryMock.Setup(repo => repo.FindByIdAsync(clientId))
+            .ReturnsAsync(existingClient);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new ClientService(clientRepositoryMock.Object, null, unitOfWorkMock.Object);
+
+        // Act
+        var result = await service.GetByIdAsync(clientId);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_NonExistingClient_ReturnsClientResponseWithErrorMessage()
+    {
+        // Arrange
+        var clientId = 1;
+        Client existingClient = null; // Simulate non-existing client
+        var clientRepositoryMock = new Mock<IClientRepository>();
+        clientRepositoryMock.Setup(repo => repo.FindByIdAsync(clientId))
+            .ReturnsAsync(existingClient);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new ClientService(clientRepositoryMock.Object, null, unitOfWorkMock.Object);
+
+        // Act
+        var result = await service.GetByIdAsync(clientId);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task SaveAsync_ValidClient_ReturnsClientResponseWithClient()
+    {
+        // Arrange
+        var newClient = new Client { Names = "Alice", LastNames = "Johnson" };
+        var clientRepositoryMock = new Mock<IClientRepository>();
+        clientRepositoryMock.Setup(repo => repo.AddAsync(newClient));
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new ClientService(clientRepositoryMock.Object, null, unitOfWorkMock.Object);
+
+        // Act
+        var result = await service.SaveAsync(newClient);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ExistingClient_ReturnsClientResponseWithUpdatedClient()
+    {
+        // Arrange
+        var clientId = 1;
+        var existingClient = new Client { Id = clientId, Names = "John", LastNames = "Doe" };
+        var updatedClient = new Client { Id = clientId, Names = "Jane", LastNames = "Smith" };
+        var clientRepositoryMock = new Mock<IClientRepository>();
+        clientRepositoryMock.Setup(repo => repo.FindByIdAsync(clientId))
+            .ReturnsAsync(existingClient);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new ClientService(clientRepositoryMock.Object, null, unitOfWorkMock.Object);
+
+        // Act
+        var result = await service.UpdateAsync(clientId, updatedClient);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_NonExistingClient_ReturnsClientResponseWithErrorMessage()
+    {
+        // Arrange
+        var clientId = 1;
+        var updatedClient = new Client { Id = clientId, Names = "Jane", LastNames = "Smith" };
+        Client existingClient = null; // Simulate non-existing client
+        var clientRepositoryMock = new Mock<IClientRepository>();
+        clientRepositoryMock.Setup(repo => repo.FindByIdAsync(clientId))
+            .ReturnsAsync(existingClient);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new ClientService(clientRepositoryMock.Object, null, unitOfWorkMock.Object);
+
+        // Act
+        var result = await service.UpdateAsync(clientId, updatedClient);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task ListAsync_ReturnsListOfClients()
+    {
+        // Arrange
+        var clients = new List<Client>
+        {
+            new Client { Id = 1, Names = "John", LastNames = "Doe" },
+            new Client { Id = 2, Names = "Jane", LastNames = "Smith" }
+        };
+        var clientRepositoryMock = new Mock<IClientRepository>();
+        clientRepositoryMock.Setup(repo => repo.ListAsync())
+            .ReturnsAsync(clients);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new ClientService(clientRepositoryMock.Object, null, unitOfWorkMock.Object);
+
+        // Act
+        var result = await service.ListAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count());
+    }
+}
---
+++ web-services-ielectric/Tests/UnitTests/TechniciansServiceTests.cs
@@ -0,0 +1,129 @@
+﻿using Xunit;
+using Moq;
+using web_services_ielectric.Shared.Domain.Repositories;
+using web_services_ielectric.Technicians.Domain.Models;
+using web_services_ielectric.Technicians.Domain.Repositories;
+using web_services_ielectric.Technicians.Services;
+
+namespace web_services_ielectric.Tests.UnitTests;
+
+public class TechniciansServiceTests
+{
+    [Fact]
+    public async Task GetByIdAsync_ExistingTechnician_ReturnsTechnicianResponseWithTechnician()
+    {
+        // Arrange
+        var technicianId = 1;
+        var existingTechnician = new Technician { Id = technicianId, Names = "John", LastNames = "Doe" };
+        var technicianRepositoryMock = new Mock<ITechnicianRepository>();
+        technicianRepositoryMock.Setup(repo => repo.FindByIdAsync(technicianId))
+            .ReturnsAsync(existingTechnician);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new TechnicianService(technicianRepositoryMock.Object, unitOfWorkMock.Object, null);
+
+        // Act
+        var result = await service.GetByIdAsync(technicianId);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_NonExistingTechnician_ReturnsTechnicianResponseWithErrorMessage()
+    {
+        // Arrange
+        var technicianId = 1;
+        Technician existingTechnician = null; // Simulate non-existing technician
+        var technicianRepositoryMock = new Mock<ITechnicianRepository>();
+        technicianRepositoryMock.Setup(repo => repo.FindByIdAsync(technicianId))
+            .ReturnsAsync(existingTechnician);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new TechnicianService(technicianRepositoryMock.Object, unitOfWorkMock.Object, null);
+
+        // Act
+        var result = await service.GetByIdAsync(technicianId);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task SaveAsync_ValidTechnician_ReturnsTechnicianResponseWithTechnician()
+    {
+        // Arrange
+        var newTechnician = new Technician { Names = "Alice", LastNames = "Johnson" };
+        var technicianRepositoryMock = new Mock<ITechnicianRepository>();
+        technicianRepositoryMock.Setup(repo => repo.AddAsync(newTechnician));
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new TechnicianService(technicianRepositoryMock.Object, unitOfWorkMock.Object, null);
+
+        // Act
+        var result = await service.SaveAsync(newTechnician);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ExistingTechnician_ReturnsTechnicianResponseWithUpdatedTechnician()
+    {
+        // Arrange
+        var technicianId = 1;
+        var existingTechnician = new Technician { Id = technicianId, Names = "John", LastNames = "Doe" };
+        var updatedTechnician = new Technician { Id = technicianId, Names = "Jane", LastNames = "Smith" };
+        var technicianRepositoryMock = new Mock<ITechnicianRepository>();
+        technicianRepositoryMock.Setup(repo => repo.FindByIdAsync(technicianId))
+            .ReturnsAsync(existingTechnician);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new TechnicianService(technicianRepositoryMock.Object, unitOfWorkMock.Object, null);
+
+        // Act
+        var result = await service.UpdateAsync(technicianId, updatedTechnician);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_NonExistingTechnician_ReturnsTechnicianResponseWithErrorMessage()
+    {
+        // Arrange
+        var technicianId = 1;
+        var updatedTechnician = new Technician { Id = technicianId, Names = "Jane", LastNames = "Smith" };
+        Technician existingTechnician = null; // Simulate non-existing technician
+        var technicianRepositoryMock = new Mock<ITechnicianRepository>();
+        technicianRepositoryMock.Setup(repo => repo.FindByIdAsync(technicianId))
+            .ReturnsAsync(existingTechnician);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new TechnicianService(technicianRepositoryMock.Object, unitOfWorkMock.Object, null);
+
+        // Act
+        var result = await service.UpdateAsync(technicianId, updatedTechnician);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task ListAsync_ReturnsListOfTechnicians()
+    {
+        // Arrange
+        var technicians = new List<Technician>
+        {
+            new Technician { Id = 1, Names = "John", LastNames = "Doe" },
+            new Technician { Id = 2, Names = "Jane", LastNames = "Smith" }
+        };
+        var technicianRepositoryMock = new Mock<ITechnicianRepository>();
+        technicianRepositoryMock.Setup(repo => repo.ListAsync())
+            .ReturnsAsync(technicians);
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var service = new TechnicianService(technicianRepositoryMock.Object, unitOfWorkMock.Object, null);
+
+        // Act
+        var result = await service.ListAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count());
+    }
+}
